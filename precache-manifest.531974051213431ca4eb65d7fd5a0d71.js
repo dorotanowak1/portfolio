54ad14e2f8cb97327efd77708ced2996d1df9d74
Dorota Nowak <dorotanowak@rocketmail.com>
2019-06-04 13:20:01 +0100

    Updates

---
+++ precache-manifest.531974051213431ca4eb65d7fd5a0d71.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7158ab85065d1c2d55209ff24c1737dc",
+    "url": "/dorotanowak1/portfolio/index.html"
+  },
+  {
+    "revision": "ac6c72655f08b0b15e54",
+    "url": "/dorotanowak1/portfolio/static/css/2.a57016ca.chunk.css"
+  },
+  {
+    "revision": "fe246912a85b099f8b15",
+    "url": "/dorotanowak1/portfolio/static/css/main.854e7e99.chunk.css"
+  },
+  {
+    "revision": "ac6c72655f08b0b15e54",
+    "url": "/dorotanowak1/portfolio/static/js/2.913cddfe.chunk.js"
+  },
+  {
+    "revision": "fe246912a85b099f8b15",
+    "url": "/dorotanowak1/portfolio/static/js/main.96097400.chunk.js"
+  },
+  {
+    "revision": "e5cb7d90c201082257fb",
+    "url": "/dorotanowak1/portfolio/static/js/runtime~main.8e963b08.js"
+  },
+  {
+    "revision": "cb31f7b8a8592321694172c0ecb23df5",
+    "url": "/dorotanowak1/portfolio/static/media/1.cb31f7b8.png"
+  },
+  {
+    "revision": "7aa576fa21f69181cd4aeb72d724c0ed",
+    "url": "/dorotanowak1/portfolio/static/media/8a.7aa576fa.jpg"
+  },
+  {
+    "revision": "3ac929532e29385ce74ccbc65dabccdd",
+    "url": "/dorotanowak1/portfolio/static/media/boardgame.3ac92953.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/dorotanowak1/portfolio/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/dorotanowak1/portfolio/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/dorotanowak1/portfolio/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/dorotanowak1/portfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/dorotanowak1/portfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "d66db2079fd8d3ff7305e6c65d65d7a5",
+    "url": "/dorotanowak1/portfolio/static/media/css.d66db207.svg"
+  },
+  {
+    "revision": "36b1abcfb3df7a81819244484a32f876",
+    "url": "/dorotanowak1/portfolio/static/media/fbS.36b1abcf.svg"
+  },
+  {
+    "revision": "407a5ded7d957eb5640a6c5ce6c66b22",
+    "url": "/dorotanowak1/portfolio/static/media/filmfestival.407a5ded.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/dorotanowak1/portfolio/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "58273e1489392aa979991a625d947c7b",
+    "url": "/dorotanowak1/portfolio/static/media/githubS.58273e14.svg"
+  },
+  {
+    "revision": "860f7afbfab5b3d33502f49d195587b5",
+    "url": "/dorotanowak1/portfolio/static/media/html.860f7afb.svg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/dorotanowak1/portfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/dorotanowak1/portfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/dorotanowak1/portfolio/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/dorotanowak1/portfolio/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/dorotanowak1/portfolio/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "484819c8f17ba43de366e8283b66bdc3",
+    "url": "/dorotanowak1/portfolio/static/media/instaS.484819c8.svg"
+  },
+  {
+    "revision": "99bc00542ca4b1e54f15d53d8df7715f",
+    "url": "/dorotanowak1/portfolio/static/media/javascript.99bc0054.svg"
+  },
+  {
+    "revision": "2ce6fb9bbccb18625f7d118cd75f48d2",
+    "url": "/dorotanowak1/portfolio/static/media/jquery.2ce6fb9b.svg"
+  },
+  {
+    "revision": "dc64c824dda5f29cd3864f9bda1fdcd8",
+    "url": "/dorotanowak1/portfolio/static/media/linkedinS.dc64c824.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/dorotanowak1/portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/dorotanowak1/portfolio/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/dorotanowak1/portfolio/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/dorotanowak1/portfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/dorotanowak1/portfolio/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "f09c420e294f602101ba22aa74240db3",
+    "url": "/dorotanowak1/portfolio/static/media/photoshop.f09c420e.svg"
+  },
+  {
+    "revision": "0289a8499045aa49bab859ea64af5180",
+    "url": "/dorotanowak1/portfolio/static/media/quote.0289a849.png"
+  },
+  {
+    "revision": "6d8a63a7e5dd4dfd579cd66592215fec",
+    "url": "/dorotanowak1/portfolio/static/media/quotes.6d8a63a7.jpeg"
+  },
+  {
+    "revision": "90552fc53de18398a934d32aec962234",
+    "url": "/dorotanowak1/portfolio/static/media/react.90552fc5.png"
+  },
+  {
+    "revision": "76b80d840090864e32ebe95523e3e5a4",
+    "url": "/dorotanowak1/portfolio/static/media/reactapp.76b80d84.jpeg"
+  },
+  {
+    "revision": "59ec478e8f71e0bc5b4b3f5264bd524b",
+    "url": "/dorotanowak1/portfolio/static/media/todoapp.59ec478e.png"
+  }
+]);
